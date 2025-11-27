abb890a7b2871ce8a4dcaa5ecc559d710f43c633
masiro918 <matias.siro@outlook.com>
2025-11-27 14:07:59 +0200

    5.1 done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
   const [user, setUser] = useState([])
   const [username, setUsername] = useState([])
   const [password, setPassword] = useState([])
+  const [errorMessage, setErrorMessage] = useState([])
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -16,14 +17,14 @@ const App = () => {
     )
   }, [])
 
-  const handleLogin = async event => {
+  const handleLogin = async (event) => {
     event.preventDefault()
 
     try {
       const user = await loginService.loginRequest({ username, password })
       setUser(user)
-      setUsername('')
-      setPassword('')
+      setUsername(user.username)
+      setPassword(user.password)
     } catch {
       setErrorMessage('wrong credentials')
       setTimeout(() => {
@@ -36,6 +37,7 @@ const App = () => {
     <form onSubmit={handleLogin}>
       <div>
         <h1>Log in to application</h1>
+        <p>{errorMessage}</p>
         <label>
           username
           <input
@@ -64,8 +66,8 @@ const App = () => {
       {!user && loginForm()}
       {user && (
         <div>
-          <p>{user.name} logged in</p>
           <h2>blogs</h2>
+          <p>{user.name} logged in</p>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
--- src/services/login.js
+++ src/services/login.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 const baseUrl = '/api/login'
 
-const loginRequest = args => {
-  axios.post(baseUrl, args).then((response) => {
-    return response.data
-  })
+const loginRequest = async args => {
+  const response = await axios.post(baseUrl, args)
+  return response.data
 }
 
 export default { loginRequest }
